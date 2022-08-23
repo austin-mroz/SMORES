dbcceb360807732c5680670797aa3a081b967b92
Lukas Turcani <lukasturcani@mailbox.org>
2022-08-23 16:08:26 +0100

    Add command line interface

--- method_tests/test_streusel.py
+++ method_tests/test_streusel.py
@@ -1,12 +1,21 @@
-import streusel
-from streusel.gaussian_cube import *
-import os
+import argparse
 
-cube = os.getcwd() + '/ethane/ethane_epot.cube'
-
-mol = Molecule(cube)
-mol.get_efield()
-mol.sample_efield()
-print(mol.vol)
+from streusel import gaussian_cube
 
 
+def _get_command_line_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cube_file")
+    return parser.parse_args()
+
+
+def main() -> None:
+    cli_args = _get_command_line_arguments()
+    mol = gaussian_cube.Molecule(cli_args.cube_file)
+    mol.get_efield()
+    mol.sample_efield()
+    print(f"VOLMUME {mol.vol}")
+
+
+if __name__ == "__main__":
+    main()
