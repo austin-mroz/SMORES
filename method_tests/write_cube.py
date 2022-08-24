86814811f27788d8b4d1ae14ca8a8a5e439c83a1
Lukas Turcani <lukasturcani@mailbox.org>
2022-08-24 20:03:30 +0100

    Make formatting match gaussian

--- method_tests/write_cube.py
+++ method_tests/write_cube.py
@@ -1,27 +1,45 @@
 import numpy as np
+import itertools
 
-def write_cube(filepath:str, vox_grid:np.ndarray, coords:np.ndarray, elements:list[str]):
+def write_cube(
+    filepath:str,
+    vox_grid:np.ndarray,
+    coords:np.ndarray,
+    elements:list[str],
+    origin: np.ndarray,
+    res: np.ndarray,
+) -> None:
+    origin_x, origin_y, origin_z = origin
+    res_x, res_y, res_z = res
+    nvox_x, nvox_y, nvox_z = vox_grid.shape
     with open(filepath, 'w') as cube:
         # write header stuff
-        cube.write('title\n')
-        cube.write('title2\n')
-        cube.write('num species >> origin xyz\n')
-        cube.write('voxel/lattice info1 \n')
-        cube.write('voxel/lattice infor \n')
-        cube.write('voxel/lattice info3 \n')
-
+        lines = [
+            ' title',
+            ' title2',
+            f'{len(elements): >5} {origin_x: >11.6f} {origin_y: >11.6f} {origin_z: >11.6f}',
+            f'{nvox_x: >5} {res_x: >11.6f} {0.: >11.6f} {0.: >11.6f}',
+            f'{nvox_y: >5} {0.: >11.6f} {res_y: >11.6f} {0.: >11.6f}',
+            f'{nvox_z: >5} {0.: >11.6f} {0.: >11.6f} {res_z: >11.6f}',
+        ]
         for element, [x, y, z] in zip(elements, coords):
-            cube.write(f'{element} 0.0 {x} {y} {z}\n')
-        
+            lines.append(f'{element: >5} {0.: >11.6f} {x: >11.6f} {y: >11.6f} {z: >11.6f}')
+
         # write voxel grid to cube file from tensor
-        nvox = np.prod(vox_grid.shape)
-        cube_vox_lines = nvox//6
-        leftover_vox = nvox%6
+        ncols = 6
+        for i in range(nvox_x):
+            for j in range(nvox_y):
+                line = []
+                for k in range(nvox_z):
+                    line.append(f'{vox_grid[i, j, k]: >12.5E}')
+                    if len(line) == ncols:
+                        lines.append(_pad_left(" ".join(line)))
+                        line = []
+                lines.append(_pad_left(" ".join(line)))
+                line = []
+        content = '\n'.join(lines)
+        cube.write(f'{content}\n')
 
-        vox_flat = vox_grid.flatten()
 
-        for k in range(0,cube_vox_lines,6):
-            cube.write(f'{vox_flat[k]} {vox_flat[k+1]} {vox_flat[k+2]} {vox_flat[k+3]} {vox_flat[k+4]} {vox_flat[k+5]}\n')
-
-        cube.write(f' '.join(map(str, vox_flat[-leftover_vox:])))
-
+def _pad_left(s: str) -> str:
+    return f" {s}"
