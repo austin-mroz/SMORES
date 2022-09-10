7a4855cf15ee2c4f96c8e3d27d84540b118f3658
Lukas Turcani <lukasturcani@mailbox.org>
2022-09-10 18:34:41 +0100

    Refactor `molecule.py`
    `molecule.py` has been optimized and type-annotated.
    
    co-authored-by: Austin Mroz <austin.mroz@gmail.com>

--- src/smores/molecule.py
+++ src/smores/molecule.py
@@ -1,16 +1,20 @@
+from __future__ import annotations
+
 import os
-import pandas as pd 
+import pathlib
+from itertools import product
+
 import numpy as np
-
-from rdkit import Chem
-from rdkit.Chem import AllChem
-
-#import sys; sys.path.insert(0,'/home/he/bin/github_clones/STREUSEL/streusel')
-#from gaussian_cube import *
-
+import numpy.typing as npt
+import pandas as pd
 import psi4
+import rdkit.Chem.AllChem as AllChem
 
-import utilities as util
+from . import utilities
+
+
+class InvalidDirectoryError(Exception):
+    pass
 
 
 class Molecule:
@@ -18,158 +22,122 @@ class Molecule:
     class to handle molecule objects for featurization
     """
 
-    def __init__(self, parent_path, calc_path):
-        self.parent_path = parent_path
-        self.calc_path = calc_path
-        if not os.path.isdir(self.calc_path):
-            os.makedirs(self.calc_path)
-
-    def init_from_xyz(self, xyz_file_path):
-        def read_xyz(filepath):
-            with open(filepath, 'r') as f:
-                lines = f.readlines()
-            df = pd.DataFrame()
-            for line in lines[2:]:
-                line = list(filter(None, line.removesuffix('\n').split(' ')))
-                nrow = pd.DataFrame([[str(line[0]), float(line[1]), float(line[2]), float(line[3])]])
-                tempdf = pd.concat([df, nrow])
-                df = pd.DataFrame(tempdf)
-            df.columns = ['atom', 'x', 'y', 'z']
-            return df
-        self.xyz = read_xyz(xyz_file_path)
-        self.elements = list(self.xyz['atom'])
-        self.coordinates = self.xyz[['x','y','z']].to_numpy()
-
-    def init_from_smiles(self, smiles):
-
-        def mk_xyz(self):
-            with open(f'{self.calc_path}sol.xyz','w') as f:
-                f.write(f'{len(self.elements)}\n')
-                f.write(f'{self.smiles}\n')
-                for element, coordinate in zip(self.elements, self.coordinates):
-                    f.write(f'{element} {coordinate[0]} {coordinate[1]} {coordinate[2]}\n')
-            
-            return f'{self.calc_path}sol.xyz'
-        self.smiles = smiles
-
-        # Create a molecule with RDKit
-        rdkit_mol = Chem.AddHs(Chem.MolFromSmiles(self.smiles))
-
-        self.elements = [a.GetSymbol() for a in rdkit_mol.GetAtoms()]
-        
-        # Generate a conformation
+    def __init__(
+        self,
+        smiles: str,
+    ) -> None:
+        rdkit_mol = AllChem.AddHs(AllChem.MolFromSmiles(smiles))
+        self._elements = [atom.GetSymbol() for atom in rdkit_mol.GetAtoms()]
         AllChem.EmbedMolecule(rdkit_mol)
-        self.coordinates = rdkit_mol.GetConformer(0).GetPositions().astype(np.float32)
-        xyz_file = mk_xyz(self)
-        self.xyz = util.read_xyz(xyz_file)
+        rdkit_coordinates = (
+            rdkit_mol.GetConformer(0).GetPositions().astype(np.float32)
+        )
+        center_of_mass = _get_center_of_mass(self._elements, rdkit_coordinates)
+        self._coordinates = rdkit_coordinates - center_of_mass
 
-    def gen_esp_cube(self, res=51, optimize=False):
-        def translate_molecule(self):
-            def get_CenterOfMass(xyz):
-                """ Returns the NG* values of the Center of Mass """
-                atomic_mass = {'H':1.01, 'He':4.00, 'Li':6.94, 'Be':9.01, 'B':10.81, 'C':12.01,
-                            'N':14.01, 'O':16.00, 'F':19.00, 'Ne':20.18, 'Na':22.99, 'Mg':24.31,
-                            'Al':26.98, 'Si':28.09, 'P':30.97, 'S':32.07, 'Cl':35.45, 'Ar':39.95,
-                            'K':39.10, 'Ca':40.08, 'Sc':44.96, 'Ti':47.87, 'V':50.94, 'Cr':52.00,
-                            'Mn':54.94, 'Fe':55.85, 'Co':58.93, 'Ni':58.69, 'Cu':63.55, 'Zn':65.39,
-                            'Ga':69.72, 'Ge':72.61, 'As':74.92, 'Se':78.96, 'Br':79.90, 'Kr':83.80,
-                            'Rb':85.47, 'Sr':87.62, 'Y':88.91, 'Zr':91.22, 'Nb':92.91, 'Mo':95.94,
-                            'Tc':98.00, 'Ru':101.07, 'Rh':102.91, 'Pd':106.42, 'Ag':107.87,
-                            'Cd':112.41, 'In':114.82, 'Sn':118.71, 'Sb':121.76, 'Te':127.60,
-                            'I':126.90, 'Xe':131.29, 'Cs':132.91, 'Ba':137.33, 'La':138.91,
-                            'Ce':140.12, 'Pr':140.91, 'Nd':144.24, 'Pm':145.00, 'Sm':150.36,
-                            'Eu':151.96, 'Gd':157.25, 'Tb':158.93, 'Dy':162.50, 'Ho':164.93,
-                            'Er':167.26, 'Tm':168.93, 'Yb':173.04, 'Lu':174.97, 'Hf':178.49,
-                            'Ta':180.95, 'W':183.84, 'Re':186.21, 'Os':190.23, 'Ir':192.22,
-                            'Pt':195.08, 'Au':196.97, 'Hg':200.59, 'Tl':204.38, 'Pb':207.2,
-                            'Bi':208.98, 'Po':209.00, 'At':210.00, 'Rn':222.00, 'Fr':223.00,
-                            'Ra':226.00, 'Ac':227.00, 'Th':232.04, 'Pa':231.04, 'U':238.03,
-                            'Np':237.00, 'Pu':244.00, 'Am':243.00, 'Cm':247.00, 'Bk':247.00,
-                            'Cf':251.00, 'Es':252.00, 'Fm':257.00, 'Md':258.00, 'No':259.00,
-                            'Lr':262.00, 'Rf':261.00, 'Db':262.00, 'Sg':266.00, 'Bh':264.00,
-                            'Hs':269.00, 'Mt':268.00}
-                total_mass = 0
-                comx = 0
-                comy = 0
-                comz = 0
+    @classmethod
+    def init_from_xyz(
+        cls,
+        path: pathlib.Path | str,
+    ) -> Molecule:
 
-                for index, row in xyz.iterrows():
-                    atom_mass = atomic_mass[str(row['atom'])]
-                    comx += atom_mass * row['x']
-                    comy += atom_mass * row['y']
-                    comz += atom_mass * row['z']
-                    total_mass += atom_mass
-                comx = comx/total_mass
-                comy = comy/total_mass
-                comz = comz/total_mass
-                com = pd.DataFrame(data=[[comx, comy, comz]])
-                com.columns = ['x', 'y', 'z']
-                return com
-            # obtain the center of mass for the molecule
-            com = get_CenterOfMass(self.xyz)
+        if isinstance(path, str):
+            path = pathlib.Path(path)
 
-            # translate the coordinates so that the center of mass is at (0,0,0)
-            translated_mol = pd.DataFrame()
-            for index, row in self.xyz.iterrows():
-                nX = row['x'] - com['x'].values[0]
-                nY = row['y'] - com['y'].values[0]
-                nZ = row['z'] - com['z'].values[0]
-                nrow = pd.DataFrame([[row['atom'], nX, nY, nZ]])
-                tempdf = pd.concat([translated_mol, nrow])
-                translated_mol = pd.DataFrame(tempdf)
-            translated_mol.columns = ['atom', 'x', 'y', 'z']
-            return translated_mol
+        # Create an instance without calling the __init__ method.
+        instance = cls.__new__(cls)
+        xyz_data = utilities.read_xyz(path)
+        instance._elements = xyz_data.elements
+        center_of_mass = _get_center_of_mass(
+            instance._elements,
+            xyz_data.coordinates,
+        )
+        instance._coordinates = xyz_data.coordinates - center_of_mass
+        return instance
 
-        def gen_voxel_grid(self, res):
-            # we want to make a box with one corner at (-5,5,5)A and one at (5,5,5)A with a resolution of 0.2 A
-            grid_xyz_coords = []
-            for i in range(res):
-                for j in range(res):
-                    for k in range(res):
-                        itrans = -5 + 0.2*i
-                        jtrans = -5 + 0.2*j 
-                        ktrans = -5 + 0.2*k
-                        grid_xyz_coords.append([itrans, jtrans, ktrans])
-            # write the grid to a .dat file
-            with open(self.calc_path + 'grid.dat', 'w') as file:
-                for xyz in grid_xyz_coords:
-                    for c in xyz:
-                        file.write(str(c) + ' ')
-                    file.write('\n')
-            self.grid = self.calc_path + 'grid.dat'
+    def _generate_voxel_grid(
+        self,
+        resolution: int,
+        output_directory: pathlib.Path,
+    ) -> None:
+        # we want to make a box with one corner at (-5,5,5)A and one at (5,5,5)A with a resolution of 0.2 A
+        grid_xyz_coords = []
+        for i, j, k in product(
+            range(resolution),
+            range(resolution),
+            range(resolution),
+        ):
+            itrans = -5 + 0.2 * i
+            jtrans = -5 + 0.2 * j
+            ktrans = -5 + 0.2 * k
+            grid_xyz_coords.append([itrans, jtrans, ktrans])
+        # write the grid to a .dat file
+        with open(output_directory.joinpath("grid.dat"), "w") as file:
+            for xyz in grid_xyz_coords:
+                for c in xyz:
+                    file.write(str(c) + " ")
+                file.write("\n")
 
-        print('translating molecule to (0,0,0')
-        trans_mol = translate_molecule(self)
-        self.elements = list(trans_mol['atom'])
-        self.coordinates = trans_mol[['x','y','z']].to_numpy()
-        
-        print('generating voxel grid')
+    def calculate_electostatic_potential(
+        self,
+        output_directory: pathlib.Path | str,
+        resolution: int = 51,
+        num_threads: int = 14,
+        optimize: bool = False,
+    ) -> None:
 
-        gen_voxel_grid(self, res)
-        os.chdir(self.calc_path)
-        print(os.getcwd())
-        #import psi4
-        psi4.set_options({'basis': 'aug-cc-pVDZ',
-                            'CUBEPROP_TASKS': ['ESP'],
-                            'CUBEPROP_FILEPATH': self.calc_path,
-                            'reference': 'uhf',
-                            })
-        psi4.core.set_num_threads(14)
+        output_directory = pathlib.Path(output_directory)
+        _create_directory(output_directory)
 
-        psi4_mol = psi4.core.Molecule.from_arrays(self.coordinates, elem=self.elements)
-        psi4.core.set_output_file(self.calc_path + 'output.dat', False)
-        self.output = self.calc_path + 'output.dat'
-        
+        self._generate_voxel_grid(resolution, output_directory)
+        original_directory = os.getcwd()  # aka OGD
+        os.chdir(output_directory)
+
+        psi4.set_options(
+            {
+                "basis": "aug-cc-pVDZ",
+                "CUBEPROP_TASKS": ["ESP"],
+                "CUBEPROP_FILEPATH": output_directory,
+                "reference": "uhf",
+            }
+        )
+        psi4.core.set_num_threads(num_threads)
+
+        psi4_mol = psi4.core.Molecule.from_arrays(
+            self._coordinates, elem=self._elements
+        )
+        psi4.core.set_output_file(
+            output_directory.joinpath("output.dat"), False
+        )
+        self.output = output_directory.joinpath("output.dat")
+
         if optimize:
-            print('optimizing!')
-            psi4.optimize('PBE',molecule=psi4_mol)
-        
-        print('calculating ESP')
-        E, wfn = psi4.prop('PBE', molecule=psi4_mol, properties=['GRID_ESP'], return_wfn=True)
+            print("optimizing!")
+            psi4.optimize("PBE", molecule=psi4_mol)
+
+        print("calculating ESP")
+        E, wfn = psi4.prop(
+            "PBE", molecule=psi4_mol, properties=["GRID_ESP"], return_wfn=True
+        )
         psi4.cubeprop(wfn)
-        self.cubefile = self.calc_path + 'ESP.cube'
-    
-        os.chdir(self.parent_path)
+
+        os.chdir(original_directory)
 
 
+def _create_directory(path: pathlib.Path) -> None:
+    if path.exists() and not path.is_dir():
+        raise InvalidDirectoryError(f"{path} is not a valid directory.")
 
+    if not path.exists():
+        path.mkdir(exist_ok=True, parents=True)
+
+
+def _get_center_of_mass(
+    elements: list[str],
+    coordinates: npt.NDArray[np.float32],
+) -> npt.NDArray[np.float32]:
+    atom_masses = np.array(
+        [utilities.atomic_mass[element] for element in elements]
+    )
+    scaled_coordinates = atom_masses * coordinates
+    return scaled_coordinates.sum(axis=1) / atom_masses.sum()
--- src/smores/utilities.py
+++ src/smores/utilities.py
@@ -1,16 +1,144 @@
-import pandas as pd
+import pathlib
+import typing
+from dataclasses import dataclass
 
-def read_xyz(filepath):
-    with open(filepath, 'r') as f:
+import numpy as np
+import numpy.typing as npt
+
+
+@dataclass(frozen=True)
+class XyzData:
+    elements: list[str]
+    coordinates: npt.NDArray[np.float32]
+
+
+def read_xyz(
+    path: pathlib.Path,
+) -> XyzData:
+
+    with open(path, "r") as f:
         lines = f.readlines()
-    df = pd.DataFrame()
+
+    elements = []
+    coordinates = []
     for line in lines[2:]:
-        line = list(filter(None, line.removesuffix('\n').split(' ')))
-        nrow = pd.DataFrame([[str(line[0]), float(line[1]), float(line[2]), float(line[3])]])
-        tempdf = pd.concat([df, nrow])
-        df = pd.DataFrame(tempdf)
-    df.columns = ['atom', 'x', 'y', 'z']
-    
-    return df
+        element, x, y, z = line.strip().split()
+        elements.append(element)
+        coordinates.append((x, y, z))
+    return XyzData(
+        elements=elements,
+        coordinates=np.array(coordinates),
+    )
 
 
+atomic_mass: typing.Final = {
+    "H": 1.01,
+    "He": 4.00,
+    "Li": 6.94,
+    "Be": 9.01,
+    "B": 10.81,
+    "C": 12.01,
+    "N": 14.01,
+    "O": 16.00,
+    "F": 19.00,
+    "Ne": 20.18,
+    "Na": 22.99,
+    "Mg": 24.31,
+    "Al": 26.98,
+    "Si": 28.09,
+    "P": 30.97,
+    "S": 32.07,
+    "Cl": 35.45,
+    "Ar": 39.95,
+    "K": 39.10,
+    "Ca": 40.08,
+    "Sc": 44.96,
+    "Ti": 47.87,
+    "V": 50.94,
+    "Cr": 52.00,
+    "Mn": 54.94,
+    "Fe": 55.85,
+    "Co": 58.93,
+    "Ni": 58.69,
+    "Cu": 63.55,
+    "Zn": 65.39,
+    "Ga": 69.72,
+    "Ge": 72.61,
+    "As": 74.92,
+    "Se": 78.96,
+    "Br": 79.90,
+    "Kr": 83.80,
+    "Rb": 85.47,
+    "Sr": 87.62,
+    "Y": 88.91,
+    "Zr": 91.22,
+    "Nb": 92.91,
+    "Mo": 95.94,
+    "Tc": 98.00,
+    "Ru": 101.07,
+    "Rh": 102.91,
+    "Pd": 106.42,
+    "Ag": 107.87,
+    "Cd": 112.41,
+    "In": 114.82,
+    "Sn": 118.71,
+    "Sb": 121.76,
+    "Te": 127.60,
+    "I": 126.90,
+    "Xe": 131.29,
+    "Cs": 132.91,
+    "Ba": 137.33,
+    "La": 138.91,
+    "Ce": 140.12,
+    "Pr": 140.91,
+    "Nd": 144.24,
+    "Pm": 145.00,
+    "Sm": 150.36,
+    "Eu": 151.96,
+    "Gd": 157.25,
+    "Tb": 158.93,
+    "Dy": 162.50,
+    "Ho": 164.93,
+    "Er": 167.26,
+    "Tm": 168.93,
+    "Yb": 173.04,
+    "Lu": 174.97,
+    "Hf": 178.49,
+    "Ta": 180.95,
+    "W": 183.84,
+    "Re": 186.21,
+    "Os": 190.23,
+    "Ir": 192.22,
+    "Pt": 195.08,
+    "Au": 196.97,
+    "Hg": 200.59,
+    "Tl": 204.38,
+    "Pb": 207.2,
+    "Bi": 208.98,
+    "Po": 209.00,
+    "At": 210.00,
+    "Rn": 222.00,
+    "Fr": 223.00,
+    "Ra": 226.00,
+    "Ac": 227.00,
+    "Th": 232.04,
+    "Pa": 231.04,
+    "U": 238.03,
+    "Np": 237.00,
+    "Pu": 244.00,
+    "Am": 243.00,
+    "Cm": 247.00,
+    "Bk": 247.00,
+    "Cf": 251.00,
+    "Es": 252.00,
+    "Fm": 257.00,
+    "Md": 258.00,
+    "No": 259.00,
+    "Lr": 262.00,
+    "Rf": 261.00,
+    "Db": 262.00,
+    "Sg": 266.00,
+    "Bh": 264.00,
+    "Hs": 269.00,
+    "Mt": 268.00,
+}
