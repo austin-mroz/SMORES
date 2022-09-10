6ce8499fe0b58763ff8c2d72f3bac32d52bd251d
austin-mroz <austin.mroz@gmail.com>
2022-09-10 22:48:19 +0100

    Update branch with validation-systems directory
    lukas/refactor-molecule now has validation-systems scripts so that the
    refactored `molecule.py` code can be tested.

---
+++ validation-systems/generate_validation_systems.py
@@ -0,0 +1,60 @@
+import glob
+import pandas as pd
+import os
+import argparse
+import smores
+from itertools import product
+
+def _get_command_line_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("substituent_csv")
+    parser.add_argument("replacement_group")
+    parser.add_argument("calculation_directory")
+    return parser.parse_args()
+
+def gen_molecule_smiles(replacement_group:str, 
+        r_group:str, 
+        substituent:str) -> str:
+    return substituent.replace(replacement_group, r_group)
+
+def optimize_and_gen_esp(substituent_name:str, 
+        r_group:str, 
+        substituent:str, 
+        replacement_group:str,
+        calculation_directory:str) -> None:
+
+    mol_smiles = gen_molecule_smiles(
+            replacement_group,
+            r_group,
+            substituent,
+            )
+    print(mol_smiles)
+    calculation_path = f'{calculation_directory}/{r_group}/{substituent_name}/'
+        
+    mol = smores.Molecule(os.getcwd(), calculation_path)
+    mol.init_from_smiles(mol_smiles)
+    mol.gen_esp_cube(optimize=True)
+
+def main() -> None:
+    cli_args = _get_command_line_arguments()
+
+    substituents = pd.read_csv(cli_args.substituent_csv)
+    r_groups = ['C', 'c1ccccc1']
+    #molecule_combos = [combo for combo in product(r_groups, list(substituents['substituent_smiles']))]
+    #print(molecule_combos)
+    #exit()
+    for molecule_combo in product(r_groups, list(substituents['substituent_smiles'])):
+        substituent_name = substituents.loc[substituents['substituent_smiles'] == molecule_combo[1]]['substituent_name'].values[0]
+        optimize_and_gen_esp(
+                substituent_name,
+                molecule_combo[0],
+                molecule_combo[1],
+                cli_args.replacement_group,
+                cli_args.calculation_directory,
+            )
+
+        print('---------------------------------')
+
+if __name__ == '__main__':
+    main()
+
