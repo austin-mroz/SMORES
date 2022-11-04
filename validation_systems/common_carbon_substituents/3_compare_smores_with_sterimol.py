bc6350bdf76a56444590a8ac0c52578892264053
Lukas Turcani <lukasturcani@mailbox.org>
2022-11-04 00:44:49 +0000

    wip

--- validation_systems/common_carbon_substituents/3_compare_smores_with_sterimol.py
+++ validation_systems/common_carbon_substituents/3_compare_smores_with_sterimol.py
@@ -1,24 +1,69 @@
 import argparse
+import enum
 import pathlib
+import textwrap
+
 import morfeus
 
 
 def main() -> None:
     args = _get_command_line_arguments()
 
-    for cube_file in args.cube_file:
-        pass
+    num_successes = 0
+    num_failures = 0
+    for xyz_file in args.xyz_files:
+        sterimol = _get_sterimol(xyz_file)
+        smores = _get_smores(xyz_file)
+        result = _get_result(smores, sterimol)
+        output = textwrap.dedent(
+            f"""
+            FILE: {xyz_file}
+                SMORES L: {smores.L_value} STERIMOL L: {sterimol.L_value}
+                SMORES B1: {smores.B_1_value} STERIMOL B1: {sterimol.B_1_value}
+                SMORES B5: {smores.B_5_value} STERIMOL B5: {sterimol.B_5_value}
+                RESULT: {result.value})
+            """
+        )
+        print(output)
+        if result == Result.SUCCESS:
+            num_successes += 1
+        else:
+            num_failures += 1
+
+
+class Result(enum.Enum):
+    SUCCESS = "SUCCESS"
+    FAILURE = "FAILURE"
+
+
+def _get_sterimol(xyz_file: pathlib.Path) -> morfeus.Sterimol:
+    elements, coordinates = morfeus.read_xyz(xyz_file)
+    return morfeus.Sterimol(element, coordinates)
+
+
+def _get_smores(xyz_file: pathlib.Path) -> morfeus.Sterimol:
+    elements, coordinates = morfeus.read_xyz(xyz_file)
+    return morfeus.Sterimol(element, coordinates)
+
+
+def _get_result(
+    smores: morfeus.Sterimol, sterimol: morfeus.Sterimol
+) -> Result:
+    pass
 
 
 def _get_command_line_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
         description="Calculate and compare SMORES and sterimol parameters.",
     )
+
+    default_input_directory = pathlib.Path(__file__).parent / "2_output"
     parser.add_argument(
-        "--cube_files",
+        "--xyz_files",
+        help="",
         nargs="+",
         type=pathlib.Path,
-        default=pathlib.Path(__file__).parent,
+        default=default_input_directory.glob("**/*.xyz"),
     )
     return parser.parse_args()
 
