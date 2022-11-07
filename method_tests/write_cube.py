4ff12c5a98ffe27c3e032393f1b79ad8dadead9e
Lukas Turcani <lukasturcani@mailbox.org>
2022-11-07 12:52:57 +0000

    update write_cube

--- method_tests/write_cube.py
+++ method_tests/write_cube.py
@@ -1,16 +1,16 @@
 import pathlib
+import typing
 
-import numpy as np
 import numpy.typing as npt
 
 
 def write_cube(
     path: pathlib.Path,
-    voxels: npt.ArrayLike,
-    positions: np.ndarray,
-    elements: list[str],
-    origin: np.ndarray,
-    res: np.ndarray,
+    voxels: npt.NDArray,
+    positions: npt.NDArray,
+    elements: typing.Sequence[str],
+    origin: npt.NDArray,
+    voxel_dimensions: npt.NDArray,
 ) -> None:
     """
     Write a ``.cube`` file.
@@ -18,12 +18,27 @@ def write_cube(
     Paramters:
 
         path:
+            The path to the file being written.
+
+        voxels:
+            A 3-D grid contaning voxel values.
+
+        positions:
+            An N x 3 matrix of atomic coordinates.
+
+        elements:
+            For each element of the molecule, its
+            elemental symbol.
+
+        voxel_dimensions:
+            The length of a single voxel in the x, y and
+            z dimensions.
 
     """
 
     origin_x, origin_y, origin_z = origin
-    res_x, res_y, res_z = res
-    nvox_x, nvox_y, nvox_z = voxels.shape
+    voxel_x_length, voxel_y_length, voxel_z_length = voxel_dimensions
+    num_voxels_x, num_voxels_y, num_voxels_z = voxels.shape
     with open(path, "w") as cube:
         # write header stuff
         lines = [
@@ -33,9 +48,18 @@ def write_cube(
                 f"{len(elements): >5} {origin_x: >11.6f} {origin_y: >11.6f} "
                 f"{origin_z: >11.6f}"
             ),
-            f"{nvox_x: >5} {res_x: >11.6f} {0.: >11.6f} {0.: >11.6f}",
-            f"{nvox_y: >5} {0.: >11.6f} {res_y: >11.6f} {0.: >11.6f}",
-            f"{nvox_z: >5} {0.: >11.6f} {0.: >11.6f} {res_z: >11.6f}",
+            (
+                f"{num_voxels_x: >5} {voxel_x_length: >11.6f} {0.: >11.6f} "
+                f"{0.: >11.6f}"
+            ),
+            (
+                f"{num_voxels_y: >5} {0.: >11.6f} {voxel_y_length: >11.6f} "
+                f"{0.: >11.6f}"
+            ),
+            (
+                f"{num_voxels_z: >5} {0.: >11.6f} {0.: >11.6f} "
+                f"{voxel_z_length: >11.6f}"
+            ),
         ]
         for element, [x, y, z] in zip(elements, positions):
             lines.append(
@@ -43,14 +67,13 @@ def write_cube(
                 f"{y: >11.6f} {z: >11.6f}"
             )
 
-        # write voxel grid to cube file from tensor
-        ncols = 6
-        for i in range(nvox_x):
-            for j in range(nvox_y):
+        num_columns = 6
+        for i in range(num_voxels_x):
+            for j in range(num_voxels_y):
                 line = []
-                for k in range(nvox_z):
+                for k in range(num_voxels_z):
                     line.append(f"{voxels[i, j, k]: >12.5E}")
-                    if len(line) == ncols:
+                    if len(line) == num_columns:
                         lines.append(_pad_left(" ".join(line)))
                         line = []
                 lines.append(_pad_left(" ".join(line)))
