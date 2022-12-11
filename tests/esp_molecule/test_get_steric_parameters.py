03603a18d9edbe1915f944e07ed3cad4086c4735
Lukas Turcani <lukasturcani@mailbox.org>
2022-12-11 21:43:30 +0000

    Add test

--- tests/esp_molecule/test_get_steric_parameters.py
+++ tests/esp_molecule/test_get_steric_parameters.py
@@ -4,16 +4,7 @@ import smores
 import smores.psi4
 
 
-def test_get_steric_parameters(tmp_path: pathlib.Path) -> None:
-    molecule = smores.rdkit_from_smiles("Br")
-    smores.psi4.calculate_electrostatic_potential(
-        molecule=molecule,
-        output_directory=tmp_path,
-        grid_origin=(-5, -5, -5),
-        grid_length=10,
-        num_voxels_per_dimension=50,
-    )
-    esp_molecule = smores.EspMolecule.from_cube_file(
-        path=tmp_path / "ESP.cube",
-    )
+def test_get_steric_parameters(datadir: pathlib.Path) -> None:
+    esp_molecule = smores.EspMolecule.from_cube_file(path=datadir / "ESP.cube")
     steric_parameters = esp_molecule.get_steric_parameters(0, 1)
+    print(steric_parameters)
