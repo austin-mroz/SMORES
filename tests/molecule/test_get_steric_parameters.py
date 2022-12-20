54d74e48f4f5726abd0c12a263b278f0a047c482
Lukas Turcani <lukasturcani@mailbox.org>
2022-12-20 17:13:42 +0000

    Fix test

--- tests/molecule/test_get_steric_parameters.py
+++ tests/molecule/test_get_steric_parameters.py
@@ -24,19 +24,14 @@ def test_smores_parameters_match_sterimol_if_same_radii_are_used(
     case_data: CaseData,
 ) -> None:
 
-    dummy_index = 0
-    attached_index = 1
     sterimol = morfeus.Sterimol(
         elements=case_data.atoms,
         coordinates=case_data.positions,
-        dummy_index=dummy_index + 1,
-        attached_index=attached_index + 1,
+        dummy_index=case_data.molecule.get_dummy_index() + 1,
+        attached_index=case_data.molecule.get_attached_index() + 1,
         radii=case_data.radii,
     )
-    params = case_data.molecule.get_steric_parameters(
-        dummy_index=dummy_index,
-        attached_index=attached_index,
-    )
+    params = case_data.molecule.get_steric_parameters()
     assert params.L == pytest.approx(sterimol.L_value, abs=1e-4)
     assert params.B1 == pytest.approx(sterimol.B_1_value)
     assert params.B5 == pytest.approx(sterimol.B_5_value)
@@ -65,6 +60,8 @@ def default_init_molecule(rdkit_molecule: rdkit.Mol) -> CaseData:
             atoms=tuple(
                 atom.GetSymbol() for atom in rdkit_molecule.GetAtoms()
             ),
+            dummy_index=0,
+            attached_index=1,
             positions=rdkit_molecule.GetConformer(0).GetPositions(),
             radii=radii,
         ),
@@ -84,7 +81,12 @@ def molecule_from_xyz_file(
         atoms=tuple(atom.GetSymbol() for atom in rdkit_molecule.GetAtoms()),
         positions=rdkit_molecule.GetConformer(0).GetPositions(),
         radii=radii,
-        molecule=smores.Molecule.from_xyz_file(xyz_file, radii),
+        molecule=smores.Molecule.from_xyz_file(
+            path=xyz_file,
+            dummy_index=0,
+            attached_index=1,
+            radii=radii,
+        ),
     )
 
 
@@ -101,7 +103,12 @@ def molecule_from_mol_file(
         atoms=tuple(atom.GetSymbol() for atom in rdkit_molecule.GetAtoms()),
         positions=rdkit_molecule.GetConformer(0).GetPositions(),
         radii=radii,
-        molecule=smores.Molecule.from_mol_file(mol_file, radii),
+        molecule=smores.Molecule.from_mol_file(
+            path=mol_file,
+            dummy_index=0,
+            attached_index=1,
+            radii=radii,
+        ),
     )
 
 
@@ -114,6 +121,8 @@ def molecule_from_smiles(rdkit_molecule: rdkit.Mol) -> CaseData:
         radii=radii,
         molecule=smores.Molecule.from_smiles(
             smiles=rdkit.MolToSmiles(rdkit_molecule),
+            dummy_index=0,
+            attached_index=1,
             positions=rdkit_molecule.GetConformer(0).GetPositions(),
             radii=radii,
         ),
